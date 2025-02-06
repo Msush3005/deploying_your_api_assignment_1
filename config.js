bb4c677e1e8c3cf9f57f09b47da0dd7fdbbc59f7
malleboina sushmitha <sushmithaengr23@gmail.com>
2025-02-06 12:00:27 +0530

    kfk

---
+++ config.js
@@ -0,0 +1,12 @@
+// Load the dotenv package
+require('dotenv').config();
+
+// Access environment variables
+const config = {
+  apiKey: process.env.API_KEY,
+  serverSecret: process.env.SERVER_SECRET,
+  isKalvian: process.env.IS_KALVIAN === 'true',
+};
+
+// Export the config object
+module.exports = config;
